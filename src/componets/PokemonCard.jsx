2180364c893c1b503a62a4b4613f65e53ffbfd96
Calmes <christophe.calmes@laplateforme.io>
2023-11-20 14:12:02 +0100

    Pokemon list array object

--- src/componets/PokemonCard.jsx
+++ src/componets/PokemonCard.jsx
@@ -1,18 +1,28 @@
 import libraryPictures from "../pictures/1.png"
 export const PokemonCard = () => {
-    // Image en ligne
-    //const picture = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png";
-    // Image local
-    const pictureLocal = libraryPictures
-    const alt = "Bulbasaur";
+  const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
+  const index = 1;
+  const picture = pokemonList[index].imgSrc;
+  const name = pokemonList[index].name
     
-  return (
-    <div>
+  return picture ?  (
+    <div>  
         <figure>
-            <img src={pictureLocal} alt={alt}/>
-            <figcaption>{alt}</figcaption>
+            <img src={picture} alt={name}/>
+            <figcaption>{`Name of Pokemon : ${name}`}</figcaption>
        </figure>
     </div>
-  )
+  ) : (
+        <p>Name of Pokemon : {name}</p>
+      )
 }
 export default PokemonCard;
