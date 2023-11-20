d645e8529723426fc381a56e17f739dd363df13e
Calmes <christophe.calmes@laplateforme.io>
2023-11-20 14:54:19 +0100

    Props pokemon ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,29 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import MyTitle from './componets/MyTitle'
 import PokemonCard from './componets/PokemonCard'
 
 function App() {
-  const [count, setCount] = useState(0)
+  //const [count, setCount] = useState(0)
+  const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
+  const index = 0;
+  const pokePicture = pokemonList[index].imgSrc;
+  const pokeName = pokemonList[index].name;
 
   return (
  
       <div>
         <MyTitle/>
-        <PokemonCard/>
+        <PokemonCard pokeName={pokeName} pokePicture={pokePicture}/>
       </div>
  
   )
--- src/componets/PokemonCard.jsx
+++ src/componets/PokemonCard.jsx
@@ -1,28 +1,18 @@
-import libraryPictures from "../pictures/1.png"
-export const PokemonCard = () => {
-  const pokemonList = [
-    {
-      name: "bulbasaur",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-      name: "mew",
-    },
-  ];
-  const index = 1;
-  const picture = pokemonList[index].imgSrc;
-  const name = pokemonList[index].name
+
+export const PokemonCard = ({pokePicture, pokeName}) => {
+  //console.log(pokemon);
+
+
     
-  return picture ?  (
+  return pokePicture ?  (
     <div>  
         <figure>
-            <img src={picture} alt={name}/>
-            <figcaption>{`Name of Pokemon : ${name}`}</figcaption>
+            <img src={pokePicture} alt={pokeName}/>
+            <figcaption>{`Name of Pokemon : ${pokeName}`}</figcaption>
        </figure>
     </div>
   ) : (
-        <p>Name of Pokemon : {name}</p>
+        <p>Name of Pokemon : {pokeName}</p>
       )
 }
 export default PokemonCard;
